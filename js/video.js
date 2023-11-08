cd3b985c6f1f41a56c26e429cac62777c1783aa7
umich-benhtsai <145412478+umich-benhtsai@users.noreply.github.com>
2023-11-07 19:26:08 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -8,3 +8,30 @@ document.querySelector("#play").addEventListener("click", function () {
   console.log("Play Video");
   video.play();
 });
+
+document.querySelector("#pause").addEventListener("click", function () {
+  console.log("Pause Video");
+  video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function () {
+  var newSpeed = video.playbackRate * 0.1;
+  console.log(`New Speed: ${newSpeed}`);
+  video.playbackRate = newSpeed;
+});
+
+document.querySelector("#faster").addEventListener("click", function () {
+  var newSpeed = video.playbackRate * 0.1;
+  console.log(`New Speed: ${newSpeed}`);
+  video.playbackRate = newSpeed;
+});
+
+document.querySelector("#skip").addEventListener("click", function () {
+  var newTime = video.currentTime + 10;
+  console.log(`New Time: ${newTime}`);
+  if (newTime <= video.duration) {
+    video.currentTime = newTime;
+  } else {
+    video.currentTime = video.duration;
+  }
+});
